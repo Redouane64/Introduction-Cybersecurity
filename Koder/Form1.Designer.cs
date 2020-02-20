9456f4035675cdcb95df1638fffb2d70039a5829
Redhouane Sobaihi <s-redouane@outlook.com>
2020-02-20 23:05:51 +0700

    Add project files.

---
+++ Koder/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Koder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            this.codeButton = new System.Windows.Forms.Button();
+            this.cipherTextBox = new System.Windows.Forms.TextBox();
+            this.plainTextBox = new System.Windows.Forms.TextBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(13, 13);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(61, 13);
+            label1.TabIndex = 0;
+            label1.Text = "Cipher Text";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(20, 49);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(54, 13);
+            label2.TabIndex = 1;
+            label2.Text = "Plain Text";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(46, 86);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(28, 13);
+            label3.TabIndex = 2;
+            label3.Text = "Text";
+            // 
+            // codeButton
+            // 
+            this.codeButton.Location = new System.Drawing.Point(198, 109);
+            this.codeButton.Name = "codeButton";
+            this.codeButton.Size = new System.Drawing.Size(75, 23);
+            this.codeButton.TabIndex = 3;
+            this.codeButton.Text = "Code";
+            this.codeButton.UseVisualStyleBackColor = true;
+            this.codeButton.Click += new System.EventHandler(this.CodeButton_Click);
+            // 
+            // cipherTextBox
+            // 
+            this.cipherTextBox.Location = new System.Drawing.Point(80, 10);
+            this.cipherTextBox.Name = "cipherTextBox";
+            this.cipherTextBox.Size = new System.Drawing.Size(193, 20);
+            this.cipherTextBox.TabIndex = 4;
+            // 
+            // plainTextBox
+            // 
+            this.plainTextBox.Location = new System.Drawing.Point(80, 46);
+            this.plainTextBox.Name = "plainTextBox";
+            this.plainTextBox.Size = new System.Drawing.Size(193, 20);
+            this.plainTextBox.TabIndex = 5;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(80, 83);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(193, 20);
+            this.textBox.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 144);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.plainTextBox);
+            this.Controls.Add(this.cipherTextBox);
+            this.Controls.Add(this.codeButton);
+            this.Controls.Add(label3);
+            this.Controls.Add(label2);
+            this.Controls.Add(label1);
+            this.Name = "MainForm";
+            this.Text = "Koder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button codeButton;
+        private System.Windows.Forms.TextBox cipherTextBox;
+        private System.Windows.Forms.TextBox plainTextBox;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Koder/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Koder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        string str;
+        byte[] key;
+        byte[] te;
+        byte[] cipherbytes;
+
+        private void CodeButton_Click(object sender, EventArgs e)
+        {
+            // cipher
+            SymmetricAlgorithm sa = TripleDES.Create();
+            sa.GenerateKey();
+            key = sa.Key;
+            sa.Mode = CipherMode.ECB;
+            sa.Padding = PaddingMode.PKCS7;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, sa.CreateEncryptor(), CryptoStreamMode.Write);
+            byte[] plainbytes = Encoding.Default.GetBytes(plainTextBox.Text);
+            cs.Write(plainbytes, 0, plainbytes.Length);
+            cs.Close();
+            cipherbytes = ms.ToArray();
+            ms.Close();
+            // show ciphered text
+            str = Encoding.Default.GetString(cipherbytes);
+            plainTextBox.Text = str;
+            // decipher
+            te = new byte[str.Length];
+            te = Encoding.Default.GetBytes(str);
+            SymmetricAlgorithm sa2 = TripleDES.Create();
+            sa2.Key = key;
+            sa2.Mode = CipherMode.ECB;
+            sa2.Padding = PaddingMode.PKCS7;
+            MemoryStream ms2 = new MemoryStream(te);
+            CryptoStream cs2 = new CryptoStream(ms2, sa2.CreateDecryptor(), CryptoStreamMode.Read);
+            byte[] plainbytes2 = new byte[te.Length];
+            cs2.Read(plainbytes2, 0, te.Length);
+            cs2.Close();
+            ms2.Close();
+            textBox.Text = Encoding.Default.GetString(plainbytes2);
+        }
+    }
+}
